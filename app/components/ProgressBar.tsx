4d5fbf81f289608ae1d1a025caf4f53f240fe9f6
D.  Paaske <102860689+Denpas404@users.noreply.github.com>
2024-10-02 13:58:19 +0200

    service done

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import Style from './ProgressBar.module.css'
+import { useEffect, useState } from 'react';
+import { fetchMeasurements } from '../services/fetchMeasurements';
 
-const ProgressBar = ({ value }: { value: number}) => {
+const ProgressBar = () => {
+    const [number, setNumber] = useState<number>(0);
 
-value = 1000000;
+    useEffect(() => {
+        const fetchData = async () => {
+            const startDateTime = new Date('2024-01-01').toISOString();
+        const stopDateTime = new Date().toISOString();
 
-const arrowPosition = (value / 7494025) * 100;
+        try {
+            const totalSum = await fetchMeasurements(startDateTime, stopDateTime);
+            if (totalSum !== null) {
+            setNumber(totalSum);
+            }
+        } catch (error) {
+            console.error('Error fetching measurements:', error);
+        }
+        };
+
+        fetchData();
+    }, []);
+    
+    //Temporary conversion of the number to a value that can be used in the progress bar 
+    let value = number * 10000;
+    
+    const arrowPosition = (value / 7494025) * 100;
 
     return (
-        <div className={Style.progress_bar}>
-            <span className={Style.start_label}>CPH</span>
-            <progress className="progress progress-success w-full h-5" value={value} max="7494025"></progress>
-            <span className={Style.end_label}>Jorden rundt</span>
-        <div className={Style.arrow} style={{ left: `${arrowPosition}%` }}></div>
-        </div>
+        <div className='flex-row justify-center text-center font-bold mt-4'>
+            <h1>Jorden rundt</h1>           
+            <div className={Style.progress_bar}>                
+                <progress className="progress progress-success w-full h-5" value={value} max="7494025"></progress>            
+                <div className={Style.arrow} style={{ left: `${arrowPosition}%` }}></div>
+            </div>
+        </div>        
 
     )
 }
